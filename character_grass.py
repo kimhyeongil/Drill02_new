56a1b3b1bee72f4a7731b83304faa166b5329e8d
kimhyeongil <alfkcjstk853@naver.com>
2023-09-21 12:19:59 +0900

    그리기 함수 추가

--- character_grass.py
+++ character_grass.py
@@ -5,10 +5,11 @@ open_canvas()
 character = load_image('character.png')
 grass = load_image('grass.png')
 
-clear_canvas_now()
-grass.draw_now(400,30)
-character.draw_now(400,90)
-delay(1)
+def render_frame(x,y):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,y)
+    delay(0.1)
 
 def run_circle():
     print('circle')
@@ -16,24 +17,16 @@ def run_circle():
     for deg in range(0, 360,5):
         x = cx + math.cos(math.radians(deg)) * r
         y = cy + math.sin(math.radians(deg)) * r
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        delay(0.1)
-
+        render_frame(x,y)
 
 def run_rect():
     print('rect')
 
     # bottom line
     for x in range(50, 750 + 1, 5):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(0.1)
-    # left line
+        render_frame(x,90) #x,y 위치에 화면을 그려주는 함수
 while (True):
-    #run_circle()
+    run_circle()
     run_rect()
     break
     
