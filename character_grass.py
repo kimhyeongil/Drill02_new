24f94a41fbb0aab323bd6f116d0d2a149318bcfe
kimhyeongil <alfkcjstk853@naver.com>
2023-09-21 12:14:32 +0900

    바닥 줄 그림

--- character_grass.py
+++ character_grass.py
@@ -20,12 +20,20 @@ def run_circle():
         grass.draw_now(400,30)
         character.draw_now(x,y)
         delay(0.1)
-    pass
+
+
 def run_rect():
     print('rect')
-    pass
+
+    # bottom line
+    for x in range(50, 750 + 1, 5):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,90)
+        delay(0.1)
+    # left line
 while (True):
-    run_circle()
+    #run_circle()
     run_rect()
     break
     
